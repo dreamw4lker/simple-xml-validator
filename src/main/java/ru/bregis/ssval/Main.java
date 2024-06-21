973ea4cffc28bacbfa7f376ca258ef3198fe8fd8
Alexander Shkirkov <shkirkov.au@ccnpp.ru>
2024-06-21 13:13:37 +0700

    Сделана передача XML и Schematron-файлов через аргументы, добавлено логирование

--- src/main/java/ru/bregis/ssval/Main.java
+++ src/main/java/ru/bregis/ssval/Main.java
@@ -7,6 +7,8 @@ import com.helger.schematron.svrl.SVRLHelper;
 import com.helger.schematron.svrl.jaxb.SchematronOutputType;
 import com.helger.xml.transform.StringStreamSource;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import java.io.File;
@@ -15,6 +17,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static SchematronOutputType validateXMLViaPureSchematron(@Nonnull final File schematronFile, String fixedXmlFile) throws Exception {
         final ISchematronResource aResSCH = SchematronResourceSCH.fromFile(schematronFile);
         if (!aResSCH.isValidSchematron())
@@ -23,16 +27,21 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            log.error("Please, define two arguments:\n1. XML document\n2. Schematron file");
+            return;
+        }
         //В Schematron минздрава не указан namespace по умолчанию.
         //Вырезаем его из прочитанного документа
-        String content = IOUtils.toString(new FileInputStream("/home/as/sval/doc.xml"), StandardCharsets.UTF_8);
+        String content = IOUtils.toString(new FileInputStream(args[0]), StandardCharsets.UTF_8);
         content = content.replaceFirst("<ClinicalDocument xmlns=\"urn:hl7-org:v3\"", "<ClinicalDocument xmlns=\"\"");
 
-        SchematronOutputType result = validateXMLViaPureSchematron(new File("/home/as/sval/schematron_125.sch"), content);
-
+        log.info("Validating... Please, wait...");
+        SchematronOutputType result = validateXMLViaPureSchematron(new File(args[1]), content);
         List<SVRLFailedAssert> assertList = SVRLHelper.getAllFailedAssertions(result);
-        for (SVRLFailedAssert obj : assertList) {
-            System.out.println(obj.getText());
+        for (int i = 0; i < assertList.size(); i++) {
+            SVRLFailedAssert failedAssert = assertList.get(i);
+            log.error("Failed assert #{}: Element: {}, rule: {}", (i + 1), failedAssert.getLocation(), failedAssert.getText());
         }
     }
 }
