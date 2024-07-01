9836f452f8ab67ccc7ed2868171b9393210c13cc
Alexander Shkirkov <shkirkov.au@ccnpp.ru>
2024-07-01 18:10:47 +0700

    Реализована валидация документов по XSD-схемам

--- src/main/java/ru/bregis/ssval/Main.java
+++ src/main/java/ru/bregis/ssval/Main.java
@@ -9,8 +9,15 @@ import com.helger.xml.transform.StringStreamSource;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXParseException;
 
 import javax.annotation.Nonnull;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
@@ -27,17 +34,36 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            log.error("Please, define two arguments:\n1. XML document\n2. Schematron file");
+        if (args.length < 3) {
+            log.error("Please, define three arguments:\n1. XML document\n2. XSD file\n3. Schematron file");
             return;
         }
+        //Читаем документ в строку
+        String content = IOUtils.toString(new FileInputStream(args[0]), StandardCharsets.UTF_8);
+        log.info("[STEP 1]: Validating document «{}» with XSD schema «{}». Please, wait...", args[0], args[1]);
+
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(new StreamSource(new File(args[1])));
+        Validator validator = schema.newValidator();
+        XmlErrorHandler xsdErrorHandler = new XmlErrorHandler();
+        validator.setErrorHandler(xsdErrorHandler);
+        validator.validate(new StreamSource(new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8))));
+
+        List<SAXParseException> saxExceptions = xsdErrorHandler.getExceptions();
+        for (int i = 0; i < saxExceptions.size(); i++) {
+            SAXParseException exception = saxExceptions.get(i);
+            log.error("-----===== Parse exception #{} =====-----", i + 1);
+            log.error("Line: {}, column: {}", exception.getLineNumber(), exception.getColumnNumber());
+            log.error("Exception: {}", exception.getMessage());
+        }
+        log.info("[STEP 1]: XSD validation finished: Document «{}» is {} for XSD «{}»", args[0], saxExceptions.size() > 0 ? "NOT VALID" : "VALID", args[1]);
+
         //В Schematron минздрава не указан namespace по умолчанию.
         //Вырезаем его из прочитанного документа
-        String content = IOUtils.toString(new FileInputStream(args[0]), StandardCharsets.UTF_8);
         content = content.replaceFirst("<ClinicalDocument xmlns=\"urn:hl7-org:v3\"", "<ClinicalDocument xmlns=\"\"");
 
-        log.info("Validating document «{}» with Schematron «{}». Please, wait...", args[0], args[1]);
-        SchematronOutputType result = validateXMLViaPureSchematron(new File(args[1]), content);
+        log.info("[STEP 2]: Validating document «{}» with Schematron «{}». Please, wait...", args[0], args[2]);
+        SchematronOutputType result = validateXMLViaPureSchematron(new File(args[2]), content);
         List<SVRLFailedAssert> assertList = SVRLHelper.getAllFailedAssertions(result);
         for (int i = 0; i < assertList.size(); i++) {
             SVRLFailedAssert failedAssert = assertList.get(i);
@@ -45,6 +71,6 @@ public class Main {
             log.error("Element: {}", failedAssert.getLocation());
             log.error("Rule: {}", failedAssert.getText());
         }
-        log.info("Validation finished: Document «{}» is {} for Schematron «{}»", args[0], assertList.size() > 0 ? "NOT VALID" : "VALID", args[1]);
+        log.info("[STEP 2]: Schematron validation finished: Document «{}» is {} for Schematron «{}»", args[0], assertList.size() > 0 ? "NOT VALID" : "VALID", args[2]);
     }
 }
